f2290bfb495214433ff1b3bbb3b76f87cfec9fd1
dpadilla <didacpadilla20@gmail.com>
2023-07-22 00:53:44 +0200

    Optimized songsList

--- src/Chords/index.ts
+++ src/Chords/index.ts
@@ -9,8 +9,14 @@ const AllSongs: Song[] = [
     AyMiDios
 ]
 
-export const AllSongsList: Song[] = AllSongs.sort((a, b) => {
-    if (a.name < b.name) return -1
-    if (a.name > b.name) return 1
-    return 0
-})
+export let AllSongsList: Song[]
+
+export const initSongsList = (): Song[] => {
+    AllSongsList = AllSongs.sort((a, b) => {
+        if (a.name < b.name) return -1
+        if (a.name > b.name) return 1
+        return 0
+    })
+
+    return AllSongsList
+}
